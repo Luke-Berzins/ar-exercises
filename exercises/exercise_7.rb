41443546a87a8317b4427be762360d7a7b1e70b8
luke-berzins <lukeberzins16@gmail.com>
2021-02-01 22:25:18 +0000

    ex7 functional

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Exercise 7 ANS"
+puts "----------"
+puts "input store name please"
+user_input = gets.chomp
+store = Store.create(name: "#{user_input}")
+store.save
+puts store.save!
